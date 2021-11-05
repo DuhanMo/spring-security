7ac1679cdf137899e4d9e75bb2e673ccd0aa3806
moduhan <devduhanmo@gmail.com>
2021-11-05 23:24:28 +0900

    feat: 프로젝트 기본틀 생성

---
+++ coresecurity/src/main/java/hello/corespringsecurity/controller/user/MessageController.java
@@ -0,0 +1,14 @@
+package hello.corespringsecurity.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+
+    @GetMapping(value = "/messages")
+    public String myPage() {
+        return "user/messages";
+    }
+}
